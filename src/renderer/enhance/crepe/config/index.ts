566998f45a2df956886c44af5d70ea1f15b50c0b
xz <1378511152@qq.com>
2025-09-06 12:13:16 +0800

    feat(crepe): 为 Crepe 组件添加增强配置和样式
    - 新增增强配置文件，为文本、列表和高级功能添加快捷缩写
    - 添加自定义容器样式，包括不同类型的容器样式和图标
    - 新增详情容器相关样式和 SVG 图标
    - 更新 Crepe 特性配置，支持新的增强功能

---
+++ src/renderer/enhance/crepe/config/index.ts
@@ -0,0 +1,98 @@
+import type { EnhanceBlockEditFeatureConfig, EnhanceCrepeFeatureConfig } from '@renderer/enhance/crepe/types'
+import { CrepeFeature } from '@milkdown/crepe'
+
+export const enhanceBlockEditConfig: EnhanceBlockEditFeatureConfig = {
+  textGroup: {
+    label: '文本',
+    text: {
+      label: '文本',
+      abbr: ['text', 'wb', 'wenben'],
+    },
+    h1: {
+      label: '一级标题',
+      abbr: ['h1', 'yjbt', 'bt1', 'yijibiaoti'],
+    },
+    h2: {
+      label: '二级标题',
+      abbr: ['h2', 'ejbt', 'bt2', 'erjibiaoti'],
+    },
+    h3: {
+      label: '三级标题',
+      abbr: ['h3', 'sjbt', 'bt3', 'sanjibiaoti'],
+    },
+    h4: {
+      label: '四级标题',
+      abbr: ['h4', 'sjbt', 'bt4', 'sijibiaoti'],
+    },
+    h5: {
+      label: '五级标题',
+      abbr: ['h5', 'wjbt', 'bt5', 'wujibiaoti'],
+    },
+    h6: {
+      label: '六级标题',
+      abbr: ['h6', 'ljbt', 'bt6', 'liujibiaoti'],
+    },
+    quote: {
+      label: '引用',
+      abbr: ['quote', 'yy', 'yinyong'],
+    },
+    divider: {
+      label: '分割线',
+      abbr: ['divider', 'fgx', 'dd', 'fengexian'],
+    },
+  },
+  listGroup: {
+    label: '列表',
+    bulletList: {
+      label: '无序列表',
+      abbr: ['bullet-list', 'lb', 'wxlb', 'wuxuliebiao'],
+    },
+    orderedList: {
+      label: '有序列表',
+      abbr: ['ordered-list', 'lb', 'yxlb', 'youxuliebiao'],
+    },
+    taskList: {
+      label: '任务列表',
+      abbr: ['task-list', 'lb', 'rwlb', 'renwuliebiao'],
+    },
+  },
+  advancedGroup: {
+    label: '高级',
+    image: {
+      label: '图片',
+      abbr: ['image', 'tp', 'photo', 'tupian'],
+    },
+    codeBlock: {
+      label: '代码块',
+      abbr: ['code-block', 'dmk', 'code', 'daimakuai'],
+    },
+    table: {
+      label: '表格',
+      abbr: ['table', 'tb', 'bg', 'biaoge'],
+    },
+    math: {
+      label: '公式',
+      abbr: ['math', 'gs', 'gongshi'],
+    },
+  },
+}
+
+export const enhanceConfig: EnhanceCrepeFeatureConfig = {
+  [CrepeFeature.CodeMirror]: {
+    searchPlaceholder: '搜索语言',
+    noResultText: '暂无匹配',
+    onCopy: (_) => {
+      // TODO 提示
+    },
+  },
+  [CrepeFeature.LinkTooltip]: {
+    onCopyLink: (_) => {
+      // TODO 提示
+    },
+  },
+  [CrepeFeature.Placeholder]: {
+    text: '不积跬步，无以至千里......',
+    mode: 'doc',
+  },
+  [CrepeFeature.BlockEdit]: enhanceBlockEditConfig,
+}
